52dd436096ad87560d6574b53d0904c533268c95
Brooks McMillin <brooks@brooksmcmillin.com>
2018-01-20 22:41:46 +0000

    Initial code

---
+++ get_all_block.py
@@ -0,0 +1,52 @@
+from web3 import Web3, HTTPProvider, IPCProvider
+
+import datetime
+import pymysql
+
+import secure
+
+web3 = Web3(IPCProvider())
+maxBlock = web3.eth.blockNumber
+
+blockNo = 0
+while blockNo <= maxBlock:
+
+    block = web3.eth.getBlock(blockNo)
+    difficulty = block.difficulty
+    gasLimit = block.gasLimit
+    timestamp = block.timestamp
+    transactions = len(block.transactions)
+    uncles = len(block.uncles)
+
+    # Change datetime format
+    date = datetime.datetime.fromtimestamp(timestamp)
+
+    print(date)
+
+    # Insert into database
+    connection = pymysql.connect(
+                host=secure.host,
+                user=secure.user,
+                password=secure.password,
+                db=secure.db)
+
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO ethereum_block (id, difficulty, gas_limit, timestamp, transaction_count, uncle_count) VALUES (" + \
+        str(blockNo) + ", " + \
+        str(difficulty) + ", " +  \
+        str(gasLimit) + ", " + \
+        "'" + str(date) + "', " + \
+        str(transactions) + ", " + \
+        str(uncles) + ")"
+
+        cursor.execute(sql)
+    connection.commit()
+
+    blockNo = blockNo + 1
+    
+    
+
+
+
+
+
---
+++ update_db.py
@@ -0,0 +1,5 @@
+from web3 import Web3, HTTPProvider, ICPProvider
+import datetime
+import pymysql 
+
+web3 = Web3(IPCProvider())
